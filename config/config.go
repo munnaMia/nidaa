8d4a9cdd159d85f5d2bd1896d8b128a9cacee2a7
munnaMia <mdmunnamia987@gmail.com>
2026-08-01 12:22:28 +0600

    Add configuration loading function and dotenv dependency

---
+++ config/config.go
@@ -0,0 +1,10 @@
+package config
+
+// load the env's to the configuration
+func loadConfig(path string)  {
+	
+}
+
+// take a path as an parameter if the path string is empty then it search 
+// for the envs on the root .env file return a pointer to configuration env's 
+// location
