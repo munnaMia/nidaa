f171b5cda2d39fd55ccf000c0f8f86e36356cfca
munnaMia <mdmunnamia987@gmail.com>
2026-08-03 16:51:42 +0600

    Implement RegisterUser method in UserUseCase for user registration with error handling

--- internal/usecase/userUserCase.go
+++ internal/usecase/userUserCase.go
@@ -1,6 +1,11 @@
 package usecase
 
-import "github.com/munnaMia/nidaa/internal/domain"
+import (
+	"context"
+	"time"
+
+	"github.com/munnaMia/nidaa/internal/domain"
+)
 
 type UserUseCase struct {
 	repo domain.UserRepository
@@ -10,4 +15,23 @@ func NewUserUseCase(r domain.UserRepository) *UserUseCase {
 	return &UserUseCase{
 		repo: r,
 	}
+}
+
+func (uc *UserUseCase) RegisterUser(ctx context.Context, username, name, email, password string) (string, *domain.User, error) {
+	user := &domain.User{
+		UserName:  username,
+		Name:      name,
+		Email:     email,
+		Password:  password, // hash the pssword latter.
+		CreatedAt: time.Now().UTC(),
+	}
+
+	err := uc.repo.Create(ctx, user)
+	if err != nil {
+		return "", nil, err
+	}
+
+	//return tokenjwt, user, err
+	return "", nil, err
+
 }
