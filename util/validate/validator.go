93dbbffada74ed4f049a209d896a08e531160e9a
munnaMia <mdmunnamia987@gmail.com>
2026-08-09 16:06:46 +0600

    feat: update Validator interface to return specific error types for email validation

--- util/validate/validator.go
+++ util/validate/validator.go
@@ -1,5 +1,7 @@
 package validate
 
+import "github.com/munnaMia/nidaa/util/responder"
+
 type PasswordRules struct {
 	MaxLength      int
 	MinLenght      int
@@ -10,7 +12,7 @@ type PasswordRules struct {
 }
 
 type Validator interface {
-	String(max int, min int, s string) error         // validate a string
-	Email(e string) (error, bool)                    // validate a email based on regexp
-	Password(p string, pRules PasswordRules) []error // ValidatePassword returns a list of missing requirements, or nil if valid.
+	String(max int, min int, s string) error                           // validate a string
+	Email(e string) error                                              // validate a email based on regexp
+	Password(p string, pRules PasswordRules) []responder.ValidationErr // ValidatePassword returns a list of missing requirements, or nil if valid.
 }
