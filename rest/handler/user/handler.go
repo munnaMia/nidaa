e45d4c8987071c6dfe329ed5d0372df247366e7e
munnaMia <mdmunnamia987@gmail.com>
2026-08-09 14:35:20 +0600

    feat: integrate validation into user handler and application startup

--- rest/handler/user/handler.go
+++ rest/handler/user/handler.go
@@ -8,12 +8,14 @@ import (
 	"github.com/munnaMia/nidaa/internal/usecase"
 	jsonhelper "github.com/munnaMia/nidaa/util/jsonHelper"
 	"github.com/munnaMia/nidaa/util/responder"
+	"github.com/munnaMia/nidaa/util/validate"
 )
 
 type Handler struct {
 	uc        *usecase.UserUseCase
 	responder responder.Responder
 	jshlp     *jsonhelper.JSONHelper
+	validate  validate.Validator
 }
 
 // create a user handler
@@ -21,11 +23,13 @@ func NewHandler(
 	uc *usecase.UserUseCase,
 	res responder.Responder,
 	jshlp *jsonhelper.JSONHelper,
+	valid validate.Validator,
 ) *Handler {
 	return &Handler{
 		uc:        uc,
 		responder: res,
 		jshlp:     jshlp,
+		validate:  valid,
 	}
 }
 
