91e8b112aa4911163df7d4414e6846a53bd3e810
munnaMia <mdmunnamia987@gmail.com>
2026-08-09 16:08:20 +0600

    feat: implement validation for register and login requests with structured error responses

--- rest/handler/user/request.go
+++ rest/handler/user/request.go
@@ -1,5 +1,12 @@
 package user
 
+import (
+	"net/http"
+
+	"github.com/munnaMia/nidaa/util/responder"
+	"github.com/munnaMia/nidaa/util/validate"
+)
+
 type registerRequest struct {
 	Name     string `json:"name"`
 	UserName string `json:"username"`
@@ -10,4 +17,54 @@ type registerRequest struct {
 type loginRequest struct {
 	Email    string `json:"email"`
 	Password string `json:"password"`
+}
+
+// default rule
+var defaultPasswordRule = validate.PasswordRules{
+	MaxLength:      100,
+	MinLenght:      8,
+	RequireUpper:   false,
+	RequireLower:   false,
+	RequireNumber:  false,
+	RequireSpecial: false,
+}
+
+// check register req. if it is a validated req then it return true and if not a valid req then it return http response and return false.
+func checkRegisterReq(w http.ResponseWriter, validate validate.Validator, responder responder.Responder, req registerRequest) bool {
+	if err := validate.String(50, 8, req.UserName); err != nil {
+		responder.SendError(w, http.StatusBadRequest, err.Error())
+		return false
+	}
+
+	if err := validate.String(255, 8, req.Name); err != nil {
+		responder.SendError(w, http.StatusBadRequest, err.Error())
+		return false
+	}
+
+	if err := validate.Email(req.Email); err != nil {
+		responder.SendError(w, http.StatusBadRequest, err.Error())
+		return false
+	}
+
+	if errs := validate.Password(req.Password, defaultPasswordRule); errs != nil {
+		responder.SendError(w, http.StatusBadRequest, "invalid password", errs...)
+		return false
+	}
+
+	return true
+}
+
+// check login req. if it is a validated req then it return true and if not a valid req then it return http response and return false.
+func checkLoginReq(w http.ResponseWriter, validate validate.Validator, responder responder.Responder, req loginRequest) bool {
+	if err := validate.Email(req.Email); err != nil {
+		responder.SendError(w, http.StatusBadRequest, err.Error())
+		return false
+	}
+
+	if errs := validate.Password(req.Password, defaultPasswordRule); errs != nil {
+		responder.SendError(w, http.StatusBadRequest, "invalid password", errs...)
+		return false
+	}
+
+	return true
 }
