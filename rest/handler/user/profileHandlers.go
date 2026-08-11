37a4403ee00a936d2f4df9b319fe13663949f967
munnaMia <mdmunnamia987@gmail.com>
2026-08-11 23:54:18 +0600

    feat: implement getCurrentUser function to retrieve and respond with user profile information

--- rest/handler/user/profileHandlers.go
+++ rest/handler/user/profileHandlers.go
@@ -1,10 +1,39 @@
 package user
 
-import "net/http"
+import (
+	"errors"
+	"net/http"
+
+	"github.com/munnaMia/nidaa/internal/domain"
+	reqctx "github.com/munnaMia/nidaa/internal/reqCtx"
+)
 
 // Retrieves profile info for the logged-in user
 func (h *Handler) getCurrentUser(w http.ResponseWriter, r *http.Request) {
-	r.Context().Value()
+	id, ok := reqctx.GetUserID(r.Context())
+	if !ok {
+		h.responder.SendError(w, http.StatusUnauthorized, "unauthorized access.")
+		return
+	}
+
+	user, err := h.uc.GetUser(r.Context(), id)
+	if err != nil {
+		if errors.Is(err, domain.ErrUserNotFound) {
+			h.responder.SendError(w, http.StatusNotFound, err.Error())
+			return
+		}
+		h.responder.SendError(w, http.StatusInternalServerError, "Internal server error")
+		return
+	}
+
+	userResponse := &userResponse{
+		Name:     user.Name,
+		UserName: user.UserName,
+		Email:    user.Email,
+	}
+
+	h.responder.SendResponse(w, http.StatusOK, userResponse, nil)
+
 }
 
 // Updates partial profile data (e.g., name, avatar, bio)
