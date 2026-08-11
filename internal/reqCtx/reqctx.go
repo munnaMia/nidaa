f6b71605df6bbc1a766ac42d18583591c4f22510
munnaMia <mdmunnamia987@gmail.com>
2026-08-11 23:53:51 +0600

    feat: implement functions to attach user to context and retrieve user ID

--- internal/reqCtx/reqctx.go
+++ internal/reqCtx/reqctx.go
@@ -1,9 +1,37 @@
 package reqctx
 
-import "context"
+import (
+	"context"
+	"strconv"
 
+	"github.com/munnaMia/nidaa/internal/domain"
+)
 
-type userCtxKey struct{}
+type jwtCtxKey struct{}
 
 // WithUser attaches the authenticated user to context
-func WithUser(ctx context.Context)
+func WithUser(ctx context.Context, payload *domain.Payload) context.Context {
+	return context.WithValue(ctx, jwtCtxKey{}, payload)
+
+}
+
+// GetUserPayload extracts the UserPayload from context
+func GetJWTPayload(ctx context.Context) (*domain.Payload, bool) {
+	jwtP, ok := ctx.Value(jwtCtxKey{}).(*domain.Payload)
+	return jwtP, ok
+}
+
+// GetUserID is a fast convenience helper to fetch just the User ID
+func GetUserID(ctx context.Context) (int64, bool) {
+	jwtP, ok := GetJWTPayload(ctx)
+	if !ok || jwtP == nil {
+		return 0, false
+	}
+
+	id, err := strconv.ParseInt(jwtP.Sub, 10, 64)
+	if err != nil {
+		return 0, false
+	}
+	
+	return id, true
+}
